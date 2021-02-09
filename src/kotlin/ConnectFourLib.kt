5e93df5ea0f1e65e22b29281966d57341815ab9e
kamalh2001 <63170492+kamalh2001@users.noreply.github.com>
2021-02-09 00:28:32 -0500

    can drop pieces onto board

--- src/kotlin/ConnectFourLib.kt
+++ src/kotlin/ConnectFourLib.kt
@@ -5,43 +5,71 @@ class ConnectFour {
     // [0, 0, 0]
     // [0, 0, 0]
     // [0, 0, 0]
+    companion object{
+        fun initGame(rows: Int, colums: Int): Game{
+            return Game(Board(rows, colums))
+        }
+    }
     class Game(val board: Board){
+        var turn: Boolean = false
+        fun pushMove(pos: Int){
+            if (board.isLegalStartPos(pos)){
+                board.putMove(pos, if (!turn) YELLOW_PIECE else RED_PIECE)
+
+                turn = !turn
+            }else{
+                println("illegal start")
+            }
+        }
     }
     class Board(val rows: Int, val colums: Int){
         private val board = ByteArray(rows * colums)
-        private val LAST_ROW = ((rows * colums) - colums)
+        private val LAST_ROW = ((rows * colums) - (colums + 1))
+        private val SQUARES = rows * colums
 
-        fun putMove(droppingFrom: Int = 2){
-            if (droppingFrom > rows){
+        fun putMove(droppingFrom: Int, color: Byte){
+            if (droppingFrom > colums){
                 return
             }else{
-                for (posistion in droppingFrom..((rows * colums) - droppingFrom + 1) step colums){
-                    if (board[droppingFrom] == YELLOW_PIECE || board[droppingFrom] == RED_PIECE){
-                        if (posistion > colums){
-                            board[posistion - colums] = YELLOW_PIECE
-                            return
-                        }else{
-                            println()
-                            //first move // illegal move
-                        }
+                for (posistion in droppingFrom..SQUARES step colums){
+                    if (board[posistion] == YELLOW_PIECE || board[posistion] == RED_PIECE){
+                        if ((posistion - colums) < 0) return
+                        board[posistion - colums] = color
+                        printBoard()
+                        return
                     }else if (posistion > LAST_ROW){
-                        board[posistion] = YELLOW_PIECE
+                        if (posistion == SQUARES){ // catches startPos = 0
+                            board[posistion - colums] = color
+                            return
+                        }
+                        board[posistion] = color
+                        return
                     }
                 }
             }
-            printBoard()
+        }
+        fun checkForWin(){
+            //this should adapt to varying win conditions. ex 4 in a row. 8 in a row etc
+        }
+        fun isLegalStartPos(pos: Int): Boolean{
+            return pos < colums
         }
         fun printBoard(){
             board.forEachIndexed { index, byte ->
-                if (index % rows == 0){
+                if (index % colums == 0){
                     print("\n" + byte)
                 }else{
                     print(byte)
                 }
             }
+            println("\n-----------------------------")
         }
     }
 }
 fun main() {
-    ConnectFour.Board(3,3).putMove()
+    val game = ConnectFour.initGame(7,6)
+    repeat(10){
+        game.pushMove(5)
+    }
+
 }
